c4354cc4bff0528c41ee156bd050ca09719d6699
Douglas Figueiredo <douglasarts@gmail.com>
2018-08-30 21:12:21 -0300

    Configuração de Rotas

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './config/ReactotronConfig';
 import {
-  Platform,
-  StyleSheet,
   Text,
-  View
 } from 'react-native';
 
-console.tron.log('hello rendering world');
-
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Rommanel - Coleçõeeeeeees</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
+const App = () => (
+  <Text>Rommanel</Text>
+);
+export default App;
